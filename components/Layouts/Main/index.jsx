cd712f406d85b9baf64a918d36771e808ec763fa
chirag bhalotia <me@chirag.codes>
2022-08-05 19:38:48 +0530

    removed unnecessary buttons

--- components/Layouts/Main/index.jsx
+++ components/Layouts/Main/index.jsx
@@ -236,14 +236,6 @@ export default function MainLayout({ children }) {
                                     </div>
                                 </form>
                             </div>
-                            <div className="ml-4 flex items-center lg:ml-6">
-                                <button
-                                    type="button"
-                                    className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-rose-600 hover:bg-rose-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
-                                >
-                                    Create
-                                </button>
-                            </div>
                         </div>
                     </div>
 
